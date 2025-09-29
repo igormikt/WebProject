00e281ae615dbf231f753882abe71172bf94eb0c
igormikt <tigormik@gmail.com>
2025-09-29 19:44:58 +0300

    фвв

--- web02/main/urls.py
+++ web02/main/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('data', views.data),
-    path('test', views.test),
     path('new', views.new)
 ]
--- web02/main/views.py
+++ web02/main/views.py
@@ -2,15 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
-
-def data(request):
-    return HttpResponse("<h1>Это страница data в проекте на Django</h1>")
-
-def test(request):
-    return HttpResponse("<h1>Это страница test в проекте на Django</h1>")
+    return render(request, 'main/index.html')
 
 def new(request):
-    return HttpResponse("<h1>Это вторая страница в проекте на Django</h1>")
+    return render(request, 'main/new.html')
+
 
 # Create your views here.
