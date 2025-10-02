81f5dbb6822813861441736e005cc14fbe31ab41
igormikt <tigormik@gmail.com>
2025-10-02 20:06:29 +0300

    add

--- web02/news/views.py
+++ web02/news/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def new(request):
+
+def home(request):
     return render(request, 'main/new.html')
 
-# Create your views here.
+
+
+
